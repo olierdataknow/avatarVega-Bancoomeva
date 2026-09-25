20b02a2841a72e515ae0dac6f5d2139ba3f1bb1e
Alexander Olier <alexander.olier@dataknow.co>
2026-09-25 15:14:09 -0500

    script despliegue

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Variables
+ACR_NAME="acrgenerala2v2"
+IMAGE_NAME="avatar-bancoomeva"
+ACR_LOGIN_SERVER="$ACR_NAME.azurecr.io"
+VERSION="1.0"
+
+# Iniciar sesión en el registro de contenedores de Azure
+az acr login --name "$ACR_NAME"
+
+# Construir la imagen Docker con tag de versión
+docker build -t "$ACR_LOGIN_SERVER/$IMAGE_NAME:$VERSION" .
+
+# Subir la imagen al ACR
+docker push "$ACR_LOGIN_SERVER/$IMAGE_NAME:$VERSION"
